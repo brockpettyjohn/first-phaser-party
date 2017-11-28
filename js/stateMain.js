20ea4a830032d4372c0f73af8becf2cef59bf02a
Brock Pettyjohn <brockpettyjohn@gmail.com>
2017-11-28 15:22:13 -0700

    yolo

--- js/stateMain.js
+++ js/stateMain.js
@@ -1,18 +1,39 @@
-const game = new Phaser.Game(900,526,Phaser.AUTO)
+const game = new Phaser.Game(900, 526, Phaser.AUTO)
 const StateMain = {
-    preload: function(){
+    preload: function () {
         this.load.image('background', 'img/forest.png')
         this.load.image('mainGuy', 'img/gorilla.png')
     },
-    create: function(){
-        this.background = this.game.add.sprite(0, 0, 'background')
-        this.mainGuy = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'mainGuy')
+    create: function () {
+        this.cursors = this.game.input.keyboard.createCursorKeys()
+        this.background = placeSprite(0, 0, 'background')
+        this.mainGuy = placeSprite(100, 100, 'mainGuy')
         this.mainGuy.anchor.setTo(0.5, 0.5)
         this.mainGuy.scale.setTo(0.5)
     },
-    update: function(){
+    update: function () {
+        if (this.cursors.up.isDown) {
+            this.mainGuy.y -= 5
+        }
+        if (this.cursors.down.isDown) {
+            this.mainGuy.y += 5
+        }
+        if (this.cursors.right.isDown) {
+            this.mainGuy.x += 5
+        }
+        if (this.cursors.left.isDown) {
+            this.mainGuy.x -= 5
+        }
         this.mainGuy.angle += 0.5
+        this.mainGuy.x += 0
+        this.mainGuy.y += 0
+
     }
+}
+
+function placeSprite(x, y, name) {
+    var sprite = game.add.sprite(x, y, name)
+    return sprite
 }
 
 

b8d34879415a3fa7c34e10ef7f0a250f37e5cdf0
Brock Pettyjohn <brockpettyjohn@gmail.com>
2017-11-29 14:05:32 -0700

    take a break

--- js/stateMain.js
+++ js/stateMain.js
@@ -14,21 +14,27 @@ const StateMain = {
         this.mainGuy = placeSprite(100, 100, 'mainGuy')
         this.mainGuy.anchor.setTo(0.5, 0.5)
         this.mainGuy.scale.setTo(0.5)
+        this.mainGuy.inputEnabled = true
+        this.mainGuy.input.onHold = true
+        this.mainGuy.events.onInputDown.add(this.houdini)
         this.background.inputEnabled = true
         this.background.input.onHold = true
-        this.background.events.onInputDown.add(() => {this.doesEventWork(this.input._x, this.input._y)}, this)
+        this.background.events.onInputDown.add(() => this.doesEventWork(this.input._x, this.input._y), this)
     },
     update: function () {
-        if (this.cursors.up.isDown) {
+        const gorillaX = this.mainGuy.x
+        const gorillaY = this.mainGuy.y
+
+        if (this.cursors.up.isDown && gorillaY > this.mainGuy.height / 2 + 2) {
             this.mainGuy.y -= this.speed
         }
-        if (this.cursors.down.isDown) {
+        if (this.cursors.down.isDown && gorillaY < this.world.bounds.height - this.mainGuy.height / 2 - 2) {
             this.mainGuy.y += this.speed
         }
-        if (this.cursors.right.isDown) {
+        if (this.cursors.right.isDown && gorillaX < 2 + this.world.bounds.width - this.mainGuy.width / 2 - 2) {
             this.mainGuy.x += this.speed
         }
-        if (this.cursors.left.isDown) {
+        if (this.cursors.left.isDown && gorillaX > this.mainGuy.width / 2 + 2) {
             this.mainGuy.x -= this.speed
         }
         this.mainGuy.angle += 0
@@ -36,10 +42,14 @@ const StateMain = {
         this.mainGuy.y += 0
 
     },
-    doesEventWork: function(x, y){
-        console.log(this.input._x)
+    doesEventWork: function (x, y) {
         this.mainGuy.x = x
         this.mainGuy.y = y
+    },
+    houdini: function () {
+        let hello = true
+        this.mainGuy = !hello
+        console.log(this.mainGuy)
     }
 }
 
